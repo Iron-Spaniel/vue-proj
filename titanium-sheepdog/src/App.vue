80d83722b32fa3d2b7a69be4b0cedce3e75191fe
Carraher, Dallas <Dallas.Carraher@cdk.com>
2020-01-11 21:49:22 -0800

    learning vue

---
+++ titanium-sheepdog/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return { fruit: ["banana", "bandana", "chemeketa", "havana"] };
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" :food="food" :key="food" v-for="food in fruit" />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
